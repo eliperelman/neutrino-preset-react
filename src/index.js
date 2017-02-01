cb88f31457a131b66d89aebb5ee9f634761813b0
helfi92 <helfi92@gmail.com>
2017-02-01 08:32:14 -0500

    Migrate to webpack 2.2.0

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ const webpackMerge = require('webpack-merge').smart;
 const path = require('path');
 const webpack = require('webpack');
 const MODULES = path.join(__dirname, 'node_modules');
-const CWD = process.cwd();
-const SRC = path.join(CWD, 'src');
-const TEST = path.join(CWD, 'test');
 const eslintLoader = preset.module.rules.find(r => r.use && r.use.includes('eslint'));
 const babelLoader = preset.module.rules.find(r => r.use && r.use.loader && r.use.loader.includes('babel'));
 
